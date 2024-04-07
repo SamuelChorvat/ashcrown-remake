d3a7bfc3ba91b9ac6f6b9e72d682ce7a719a4076
Samuel Chorvat <samchorvat@gmail.com>
2024-04-07 19:57:18 +0100

    Update EndTurnValidator.cs

--- src/Ashcrown.Remake.Core/Battle/Models/Dtos/Inbound/Validators/EndTurnValidator.cs
+++ src/Ashcrown.Remake.Core/Battle/Models/Dtos/Inbound/Validators/EndTurnValidator.cs
@@ -6,8 +6,7 @@ public class EndTurnValidator : AbstractValidator<EndTurn>
 {
     public EndTurnValidator()
     {
-        RuleFor(x => x.SpentEnergy)
-            .NotNull();
+        RuleFor(x => x.SpentEnergy).NotNull();
         RuleFor(x => x.SpentEnergy!.Length).Equal(4);
         RuleForEach(x => x.EndTurnAbilities).SetValidator(new EndTurnAbilityValidator());
     }
