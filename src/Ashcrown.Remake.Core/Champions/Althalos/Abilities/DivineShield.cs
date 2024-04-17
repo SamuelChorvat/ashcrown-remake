bcad0c42961cb87b6cbbb0022723ecab0ad045d8
Samuel Chorvat <36892499+SamuelChorvat@users.noreply.github.com>
2024-04-17 14:14:53 +0100

    StandardInvulnerability

--- src/Ashcrown.Remake.Core/Champions/Althalos/Abilities/DivineShield.cs
+++ src/Ashcrown.Remake.Core/Champions/Althalos/Abilities/DivineShield.cs
@@ -1,30 +1,10 @@
-﻿using Ashcrown.Remake.Core.Ability.Enums;
-using Ashcrown.Remake.Core.Ability.Extensions;
+﻿using Ashcrown.Remake.Core.Ability.Abstract;
 using Ashcrown.Remake.Core.Champion.Interfaces;
 using Ashcrown.Remake.Core.Champions.Althalos.Champion;
 
 namespace Ashcrown.Remake.Core.Champions.Althalos.Abilities;
 
-public class DivineShield : Ability.Abstract.Ability
-{
-    public DivineShield(IChampion champion) : base(champion, 
-        AlthalosConstants.DivineShield, 
-        4,
-        [0,0,0,0,1],
-        [AbilityClass.Strategic, AbilityClass.Instant], 
-        AbilityTarget.Self, 
-        AbilityType.AllyBuff)
-    {
-        Duration1 = 1;
-        Description = $"This ability makes {AlthalosConstants.Althalos} " +
-                      $"{"invulnerable".HighlightInPurple()} for {Duration1} turn.";
-        SelfCast = true;
-        Invulnerability = true;
-        TypeOfInvulnerability = [AbilityClass.All];
-        Helpful = true;
-        Buff = true;
-        ActiveEffectOwner = AlthalosConstants.Althalos;
-        ActiveEffectName = AlthalosConstants.DivineShieldActiveEffect;
-        AiStandardSelfInvulnerability = true;
-    }
-}
+public class DivineShield(IChampion champion) : StandardInvulnerability(champion,
+    AlthalosConstants.Althalos,
+    AlthalosConstants.DivineShield,
+    AlthalosConstants.DivineShieldActiveEffect);
---
+++ src/Ashcrown.Remake.Core/Champions/Eluard/Champion/EluardConstants.cs
@@ -0,0 +1,16 @@
+namespace Ashcrown.Remake.Core.Champions.Eluard.Champion;
+
+public static class EluardConstants
+{
+    public const string Eluard = "Eluard";
+    public const string Title = "The Dragon Knight";
+
+    public const string Devastate = "Devastate";
+    public const string Evade = "Evade";
+    public const string SwordStrike = "Sword Strike";
+    public const string UnyieldingWill = "Unyielding Will";
+
+    public const string DevastateActiveEffect = "DevastateActiveEffect";
+    public const string EvadeActiveEffect = "EvadeActiveEffect";
+    public const string UnyieldingWillActiveEffect = "UnyieldingWillActiveEffect";
+}
