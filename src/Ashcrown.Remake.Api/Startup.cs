92b5015dab9b9b45a559df5ec268e68033e4cc6b
Samuel Chorvat <samchorvat@gmail.com>
2024-04-24 20:48:07 +0100

    add empty api

---
+++ src/Ashcrown.Remake.Api/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace Ashcrown.Remake.Api;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddControllers();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+    }
+}
