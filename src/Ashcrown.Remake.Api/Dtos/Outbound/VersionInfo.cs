9f8af4c2736a488962e1e28622ec21c119a3e88e
Samuel Chorvat <samchorvat@gmail.com>
2024-04-27 23:07:06 +0100

    Update VersionInfo.cs

--- src/Ashcrown.Remake.Api/Dtos/Outbound/VersionInfo.cs
+++ src/Ashcrown.Remake.Api/Dtos/Outbound/VersionInfo.cs
@@ -1,7 +1,10 @@
-﻿namespace Ashcrown.Remake.Api.Dtos.Outbound;
+﻿using Ashcrown.Remake.Core.Champion;
+
+namespace Ashcrown.Remake.Api.Dtos.Outbound;
 
 public class VersionInfo
 {
     public string ServerVersion { get; init; } = AshcrownApiConstants.ServerVersion;
     public string MinimumClientVersion { get; init; } = AshcrownApiConstants.MinimumClientVersion;
+    public string[] PlayableChampions { get; init; } = ChampionConstants.AllChampionsNames;
 }
