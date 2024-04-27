94e94a4b27bc8d228783144bc40e176fff145361
Samuel Chorvat <samchorvat@gmail.com>
2024-04-27 22:50:57 +0100

    Refresh Session

--- src/Ashcrown.Remake.Api/Controllers/LoginController.cs
+++ src/Ashcrown.Remake.Api/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Ashcrown.Remake.Api.Dtos.Outbound;
+using Ashcrown.Remake.Api.Models;
 using Ashcrown.Remake.Api.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,16 +18,21 @@ public class LoginController(IPlayerSessionService playerSessionService) : Contr
     }
 
     [HttpPost("session/create")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(PlayerSession), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
-    public async Task<ActionResult> CreatePlayerSession([FromBody] string playerName)
+    public async Task<ActionResult<PlayerSession>> CreatePlayerSession([FromBody] string playerName)
     {
-        if (await playerSessionService.CreateSession(playerName))
+        if (!await playerSessionService.CreateSession(playerName))
+            return Conflict($"The playerName '{playerName}' is currently taken.");
+        
+        var playerSession = await playerSessionService.GetSession(playerName);
+        if (playerSession != null)
         {
-            return Created();
+            return playerSession;
         }
 
-        return Conflict($"The playerName '{playerName}' is currently taken.");
+        return StatusCode(500, "Something went wrong!");
+
     }
     
     [HttpGet("session/list", Name = nameof(ListCurrentSessionNames))]
@@ -34,5 +40,14 @@ public class LoginController(IPlayerSessionService playerSessionService) : Contr
     public Task<IList<string>> ListCurrentSessionNames()
     {
         return playerSessionService.GetCurrentInUsePlayerNames();
+    }
+    
+    [HttpGet("session/refresh/{playerName}", Name = nameof(RefreshSession))]
+    [ProducesResponseType(typeof(ActionResult), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PlayerSession>> RefreshSession(string playerName)
+    {
+        await playerSessionService.UpdateSession(playerName, session => session.LastRequestDateTime = DateTime.UtcNow);
+        var playerSession = await playerSessionService.GetSession(playerName);
+        return Ok(playerSession);
     }
 }
--- src/Ashcrown.Remake.Api/Models/PlayerSession.cs
+++ src/Ashcrown.Remake.Api/Models/PlayerSession.cs
@@ -1,6 +1,14 @@
-﻿namespace Ashcrown.Remake.Api.Models;
+﻿using Ashcrown.Remake.Core.Champions.Althalos.Champion;
+using Ashcrown.Remake.Core.Champions.Eluard.Champion;
+using Ashcrown.Remake.Core.Champions.Sarfu.Champion;
+
+namespace Ashcrown.Remake.Api.Models;
 
 public class PlayerSession
 {
     public DateTime LastRequestDateTime { get; set; } = DateTime.UtcNow;
+    public string IconName { get; set; } = AshcrownApiConstants.IconNames.OrderBy(x => Guid.NewGuid()).First();
+    public string CrownName { get; set; } = AshcrownApiConstants.CrownNames.OrderBy(x => Guid.NewGuid()).First();
+    public string[] BlindChampions { get; set; } = 
+        [AlthalosConstants.Name, EluardConstants.Name, SarfuConstants.Name];
 }
