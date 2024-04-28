bf1f7e06847c1b8e4e380940f2c773d994c7fa46
Samuel Chorvat <samchorvat@gmail.com>
2024-04-28 20:57:40 +0100

    Update ChampionConstants.cs

--- src/Ashcrown.Remake.Core/Champion/ChampionConstants.cs
+++ src/Ashcrown.Remake.Core/Champion/ChampionConstants.cs
@@ -1,3 +1,4 @@
+using Ashcrown.Remake.Core.Champions.Akio.Champion;
 using Ashcrown.Remake.Core.Champions.Althalos.Champion;
 using Ashcrown.Remake.Core.Champions.Eluard.Champion;
 using Ashcrown.Remake.Core.Champions.Sarfu.Champion;
@@ -8,5 +9,6 @@ public static class ChampionConstants
 {
     public const int ChampionMaxHealth = 100;
     public const int MaxNumberOfCurrentAbilities = 4;
-    public static string[] AllChampionsNames = [AlthalosConstants.Name, EluardConstants.Name, SarfuConstants.Name];
+    public static readonly string[] AllChampionsNames = [AkioConstants.Name,
+        AlthalosConstants.Name, EluardConstants.Name, SarfuConstants.Name];
 }
