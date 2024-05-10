ac505902d6566aaa2fcf4eeb1e7f931d372df8fa
Samuel Chorvat <samchorvat@gmail.com>
2024-05-10 22:44:40 +0100

    Update DraftService.cs

--- src/Ashcrown.Remake.Api/Services/DraftService.cs
+++ src/Ashcrown.Remake.Api/Services/DraftService.cs
@@ -27,10 +27,21 @@ public class DraftService(IBattleService battleService) : IDraftService
     public Task StartBattle(Guid matchId)
     {
         _draftMatches.TryGetValue(matchId, out var draftMatch);
-        if (draftMatch != null)
-        {
-            battleService.AddAcceptedMatch(matchId, draftMatch.FoundMatch, draftMatch);
-        }
+        if (draftMatch == null || battleService.IsAcceptedMatch(matchId)) return Task.CompletedTask;
+        var playerIndex = draftMatch.DraftLogic!.Players[0].Equals(draftMatch.FoundMatch.PlayerNames[0]) ? 0 : 1;
+        draftMatch.FoundMatch.PlayerBlindChampions[playerIndex] =
+        [
+            draftMatch.DraftLogic!.PickedChampions[0, 0],
+            draftMatch.DraftLogic!.PickedChampions[0, 1],
+            draftMatch.DraftLogic!.PickedChampions[0, 2]
+        ];
+        draftMatch.FoundMatch.PlayerBlindChampions[1 - playerIndex] =
+        [
+            draftMatch.DraftLogic!.PickedChampions[1, 0],
+            draftMatch.DraftLogic!.PickedChampions[1, 1],
+            draftMatch.DraftLogic!.PickedChampions[1, 2]
+        ];
+        battleService.AddAcceptedMatch(matchId, draftMatch.FoundMatch, draftMatch);
         return Task.CompletedTask;
     }
 }
