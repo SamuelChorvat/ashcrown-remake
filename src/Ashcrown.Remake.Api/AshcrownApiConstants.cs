b4ddd1ce1ff46e6b8c52e2720a8f51e41eb82461
Samuel Chorvat <36892499+SamuelChorvat@users.noreply.github.com>
2024-05-13 10:25:19 +0100

    Update AshcrownApiConstants.cs

--- src/Ashcrown.Remake.Api/AshcrownApiConstants.cs
+++ src/Ashcrown.Remake.Api/AshcrownApiConstants.cs
@@ -2,7 +2,7 @@
 
 public static class AshcrownApiConstants
 {
-    public const string ServerVersion = "0.0.0";
+    public const string ServerVersion = "2.0.0";
     public const string MinimumClientVersion = "2.0.0";
 
     public const int TimeToAcceptMatchFoundSeconds = 10;
